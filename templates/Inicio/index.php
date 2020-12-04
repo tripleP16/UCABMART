3f1cb1d0b39b19185b81ef7ac2ac74709be8fd11
Pablo Perez <perez51160900@gmail.com>
2020-12-04 00:16:26 -0400

    navbar actualizacion

--- templates/Inicio/index.php
+++ templates/Inicio/index.php
@@ -1,14 +1,13 @@
+
 <div class="row">
-    <div class="col s2"></div>
-    <div class="center col s8 white-text" id="imagenContenedor"> 
+    <div class="center col s12 white-text" id="imagenContenedor"> 
         <div class="slogan">
             <h4>Nuestra mision es la distribucion de Productos con excelencia en precios</h4>
         </div>
         <br>    
         <a class="waves-effect yellow accent-2 btn-large black-text">Comprar Ahora</a> <!--Colocar Link -->
     </div>
-    <div class="col s2 "></div>
-</div> 
+</div>
 <div class="row">
 <div class="col s1 "></div>
     <div class="col s4" id="tiendas">
