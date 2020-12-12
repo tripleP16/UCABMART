1a1e85034191e5b86709ed883734220c163fec1a
Pablo Perez <perez51160900@gmail.com>
2020-12-12 16:39:52 -0400

    carousel

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     $('select').formSelect();
     $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+          
   });
 
  
