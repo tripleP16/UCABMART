6698d8803ef0a86f24a39c69426b000e958ebd77
CRONOXT <59267988+CRONOXT@users.noreply.github.com>
2021-02-08 17:35:43 -0400

    Otro mas

--- templates/ProductoNotimart/add.php
+++ templates/ProductoNotimart/add.php
@@ -5,39 +5,22 @@
  */
 ?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Producto Notimart'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="productoNotimart form content">
-            <?= $this->Form->create($productoNotimart) ?>
+<?= $this->Form->create($productoNotimart, array('class' =>'col s8 offset-s2 formulario')) ?>
+    <div class="row formularioCont">   
+        <h5 class="center">Agregar Producto</h5>
             <fieldset>
-                <legend><?= __('Add Producto Notimart') ?></legend>
                 <?php
-                    echo $this->Form->control('prod_not_descuento');
-                    echo $this->Form->control('prod_not_fecha_inicio');
-                    echo $this->Form->control('prod_not_fecha_FIN');
+                    echo $this->Form->control('prod_not_descuento',array('label'=>'Descuento'));
+                    echo $this->Form->control('prod_not_fecha_inicio',array('label'=>'Fecha de inicio'));
+                    echo $this->Form->control('prod_not_fecha_FIN',array('label'=>'Fecha final'));
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Registrar Proveedor'), array('class'=> 'waves-effect waves-light btn-large black-text ')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
 <script>
 
-$(document).ready( function () {
-    $('#tiendas').DataTable({
-        
-    });
-    $('select').formSelect();
-    $('input').css('border-top','0px')
-    $('input').css('border-left','0px')
-    $('input').css('border-right','0px')
-} );
-</script>
 
 
